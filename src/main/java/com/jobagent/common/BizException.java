90fce76fe7d5c6a4f6f42aae8574ac4373c508ca
xiaoku <930642104@qq.com>
2026-08-13 04:36:34 +0800

    feat: 添加统一返回体与全局异常处理

---
+++ src/main/java/com/jobagent/common/BizException.java
@@ -0,0 +1,17 @@
+package com.jobagent.common;
+
+import lombok.Getter;
+
+@Getter
+public class BizException extends RuntimeException {
+    private final int code;
+
+    public BizException(String message) {
+        this(500, message);
+    }
+
+    public BizException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
